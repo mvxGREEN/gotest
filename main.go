48b30badea185d566c0458182e8b06f373a31146
xxxgreen <mvxgreen@gmail.com>
2017-11-22 02:34:43 -0600

    solved errors preventing build

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+  "github.com/julienschmidt/httprouter"
   "log"
   "fmt"
   "net/http"
-  "github.com/julienschmidt/httprouter"
 )
 
-func handleGreeting(w http.ResponseWriter, r *http.Request) {
+func handleGreeting(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
   fmt.Fprintf(w, "Hello")
 }
 
 func main () {
-  router := httprouter
+  router := httprouter.New()
   router.GET("/greeting", handleGreeting)
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
