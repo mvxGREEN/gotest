b21f5a056e1f0b43c1e0ccf51019cec7bfeff75b
xxxgreen <mvxgreen@gmail.com>
2017-11-25 02:36:20 -0600

    added multiple subdomains

--- main.go
+++ main.go
@@ -7,12 +7,32 @@ import (
   "net/http"
 )
 
-func handleGreeting(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  fmt.Fprintf(w, "Hello")
+func Index(w http.ResponseWriter, r *http.Request,
+  _ httprouter.Params) {
+  fmt.Fprintf(w, "Home Page\nAmerican Football\nBasketball\nSoccer")
+
+}
+
+func FootballStreams(w http.ResponseWriter, r *http.Request,
+  _ httprouter.Params) {
+  fmt.Fprintf(w, "American Football Streaming:\n*insert links*")
+}
+
+func BasketballStreams(w http.ResponseWriter, r *http.Request,
+  _ httprouter.Params) {
+  fmt.Fprintf(w, "Basketball Streaming:\n*insert links*")
+}
+
+func SoccerStreams(w http.ResponseWriter, r *http.Request,
+_ httprouter.Params) {
+  fmt.Fprintf(w, "Football (Soccer) Streaming:\n*insert links*")
 }
 
 func main () {
   router := httprouter.New()
-  router.GET("/greeting", handleGreeting)
-  log.Fatal(http.ListenAndServe(":8080", nil))
+  router.GET("/", Index)
+  router.GET("/football-streams", FootballStreams)
+  router.GET("/basketball-streams", BasketballStreams)
+  router.GET("/soccer-streams", SoccerStreams)
+  log.Fatal(http.ListenAndServe(":8080", router))
 }
